11e711f0ee06289ba4a49b3131e5d35e0f317959
DavidGarcia72 <42687321+DavidGarcia72@users.noreply.github.com>
2018-10-31 21:12:59 -0700

    Add files via upload

--- Examen 2/ParraInc/software/urls.py
+++ Examen 2/ParraInc/software/urls.py
@@ -6,8 +6,8 @@ from software import views
 app_name="software"
 
 urlpatterns = [
-    path('',views.index, name="index_view"),
-    path('createSoft',views.createSoft.as_view(), name="createSoft_view"),
-    path('createDepa',views.createDepa.as_view(), name="createdepa_view"),
-    path('listSoft',views.listSoft.as_view(), name="list_view"),
+    path('index/',views.index, name="index_view"),
+    path('createSoft/',views.createSoft.as_view(), name="createSoft_view"),
+    path('createDepa/',views.createDepa.as_view(), name="createdepa_view"),
+    path('listSoft/',views.listSoft.as_view(), name="list_view"),
 ]
--- Examen 2/ParraInc/software/views.py
+++ Examen 2/ParraInc/software/views.py
@@ -10,18 +10,17 @@ def index(request):
 class createSoft(generic.CreateView):
     template_name = "createSoft.html"
     model = Software
-    
+
     fields = ["nombreSoftware", "funcion","departamento"]
-    success_url = "/index.html/"
+    success_url = "/index/"
 
 class createDepa(generic.CreateView):
     template_name = "createDepa.html"
     model = Departamento
     fields = ["depa"]
-    success_url = "/index.html/"
+    success_url = "/index/"
 
 class listSoft(generic.ListView):
     template_name = "listSoft.html"
     model = Software
     fields = ["nombreSoftware", "funcion","departamento"]
-    success_url = "/index.html/"
